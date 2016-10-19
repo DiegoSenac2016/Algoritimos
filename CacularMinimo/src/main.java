80148e43e4ca320801d25b77be2200b928f0a6ce
tempor <tempor@L306-16.senacrjedu.local>
2016-10-19 21:56:04 -0200

    Cacular valor minimo

---
+++ CacularMinimo/src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		int[] a = new int[3];
+		a[0] = 70;
+		a[1] = 30;
+		a[2] = 20;
+		int valorMinimo = util.min(a);
+		System.out.println(valorMinimo);
+	}
+
+}
---
+++ CacularMinimo/src/util.java
@@ -0,0 +1,16 @@
+
+public class util {
+
+	public static int min(int [] array ) {
+		int min= array [0];
+		for (int i = 0; i< array.length; i++) {
+			if (array [i] < min) {
+				min = i;
+				
+			}
+		}
+		return min;
+		
+	}
+
+}
